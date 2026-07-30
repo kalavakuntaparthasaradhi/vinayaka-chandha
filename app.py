d5ba36f1ae32fd3bb14b08c624e980cf1257e613
parthasaradhi <kalavakuntaparthasaradhi@gmail.com>
2026-07-30 08:19:20 +0530

    check

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def login():
 def dashboard():
 
     if request.method == "POST":
-
+        print(request.form)
         name = request.form["name"]
         mobile = request.form["mobile"].replace("+91", "").replace(" ", "")
         area = request.form["area"]
