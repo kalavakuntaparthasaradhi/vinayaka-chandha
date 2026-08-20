59df3163c5222711ddf679c66a9cbc589b093582
parthasaradhi <kalavakuntaparthasaradhi@gmail.com>
2026-08-20 18:02:52 +0530

    push

--- generate_qr.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-upi_link = "upi://pay?pa=6305777042@axl&pn=Lakshmi%20Ganapathi%20Youth&cu=INR"
-
-qr = qrcode.make(upi_link)
-
-qr.save("static/upi_qr.png")
-
-print("QR code created successfully!")
